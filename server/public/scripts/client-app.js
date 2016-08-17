7994fe536b4c736f6778d11b62d783af496b060d
kerij <keriajackson@gmail.com>
2016-08-17 15:31:52 -0500

    after wednesday lectures

--- server/public/scripts/client-app.js
+++ server/public/scripts/client-app.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
 
   // add a book
   $('#book-submit').on('click', postBook);
+  $('#book-list').on('click', '.update', putBook);
+  $('#book-list').on('click', '.delete', deleteBook);
 });
 /**
  * Retrieve books from server and append to DOM
@@ -14,12 +16,24 @@ function getBooks() {
     success: function (books) {
       console.log('GET /books returns:', books);
       books.forEach(function (book) {
-        var $el = $('<li></li>');
-        $el.append('<strong>' + book.title + '</strong>');
-        $el.append(' <em>' + book.author + '</em>');
-        $el.append(' <time>' + book.published + '</time>');
-        $el.append('  ' + book.edition);
-        $el.append('  ' + book.publisher);
+        var $el = $('<div></div>');
+        var bookProperties = ['title', 'author', 'published', 'genre'];
+        bookProperties.forEach(function (property){
+          var inputType = 'text';
+          if (property == 'published') {
+            // inputType = 'date';
+            // book[property] = new Date(book[property]);
+
+          }
+          var $input = $('<input type="text" id="' + property + '"name="' + property + '" />');
+          $input.val(book[property]);
+          $el.append($input);
+        })
+
+        $el.data('bookId', book.id)
+        $el.append('<button class="update">Update</button>');
+        $el.append('<button class="delete">Delete</button>');
+
         $('#book-list').append($el);
       });
     },
@@ -55,4 +69,47 @@ function postBook() {
       console.log('POST /books does not work...');
     },
   });
+}
+
+function putBook() {
+  var book = {};
+  var inputs = $(this).parent().children().serializeArray();
+  $.each(inputs, function(i, field) {
+    book[field.name] = field.value;
+  });
+
+  console.log('book we are putting', book);
+
+  var bookId = $(this).parent().data('bookId');
+  console.log(bookId);
+
+  $.ajax({
+    type: 'PUT',
+    url: '/books/' + bookId,
+    data: book,
+    success: function () {
+      $('#book-list').empty();
+      getBooks();
+    },
+    error: function() {
+      console.log('Error PUT /books/' + bookId);
+    },
+  });
+}
+
+function deleteBook() {
+  var bookId = $(this).parent().data('bookId');
+
+  $.ajax({
+    type: 'DElETE',
+    url: '/books/' + bookId,
+    success: function () {
+      console.log('DELETE success');
+      $('#book-list').empty();
+      getBooks();
+    },
+    error: function () {
+      console.log('DELETE failed');
+    }
+  })
 }
--- server/routes/books.js
+++ server/routes/books.js
@@ -30,17 +30,73 @@ router.post('/', function(req, res){
       res.sendStatus(500);
     }
 
-    client.query('INSERT INTO books(author, title, published, edition, publisher)'
-                + 'VALUES ($1, $2, $3, $4, $5)',
-                [book.author, book.title, book.published, book.edition, book.publisher],
+    client.query('INSERT INTO books(author, title, published, edition, publisher, genre)'
+                + 'VALUES ($1, $2, $3, $4, $5, $6)',
+                [book.author, book.title, book.published, book.edition, book.publisher, book.genre],
                 function (err, result) {
                   done();
 
                   if (err) {
+                    console.log(err);
                     res.sendStatus(500);
+                  } else {res.sendStatus(201);
                   }
 
-                  res.sendStatus(201);
+
+                });
+  });
+});
+
+router.put('/:id', function(req, res){
+  var id = req.params.id;
+  var book = req.body;
+
+  pg.connect(connectionString, function(err, client, done) {
+    if (err) {
+      res.sendStatus(500);
+    }
+
+    client.query('UPDATE books' +
+                ' SET title = $1, ' +
+                ' author = $2,' +
+                ' published = $3,' +
+                ' edition = $4,' +
+                ' publisher = $5,' +
+                ' genre = $6' +
+                ' WHERE id = $7',
+              [book.title, book.author, book.published, book.edition, book.publisher, book.genre, id],
+            function (err, result){
+              done();
+              if (err) {
+                console.log('err', err);
+                res.sendStatus(500);
+              } else {
+                res.sendStatus(200);
+              }
+            });
+  });
+});
+
+router.delete('/:id', function (req, res) {
+  var id = req.params.id;
+
+  pg.connect(connectionString, function (err, client, done) {
+    if (err) {
+      res.sendStatus(500);
+    }
+
+    client.query('DELETE FROM books ' +
+                'WHERE id = $1',
+                [id],
+                function (error, result) {
+                  done();
+
+                  if (err) {
+                    res.sendStatus(500);
+                    return;
+                  }
+
+                  res.sendStatus(200);
                 });
   });
 });
